8b04edcc28e382846969315dce3277f833c99b1e
SuDelk <sudul.fernando@gmail.com>
2024-09-24 22:29:49 +0530

    removed layout

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,41 +1,26 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
 import {
-    Landing,
-    AdminDashboard,
-    CSRDashboard,
-    VendorDashboard
+  Landing,
+  AdminDashboard,
+  CSRDashboard,
+  VendorDashboard,
 } from "../pages";
 
-import {
-    NavBar,
-    Footer
-} from "../components";
-
-function Layout({ children }) {
-    const location = useLocation();
-    const hideNavbarFooter = location.pathname === "/";
-
-    return (
-        <>
-            {!hideNavbarFooter && <NavBar />}
-            {children}
-            {!hideNavbarFooter && <Footer />}
-        </>
-    );
-}
-
 export default function AppRoutes() {
-    return (
-        <Router>
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/AdminDashboard" element={<AdminDashboard />} />
-                    <Route path="/CSRDashboard" element={<CSRDashboard />} />
-                    <Route path="/VendorDashboard" element={<VendorDashboard />} />
-                </Routes>
-            </Layout>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
+        <Route path="/CSRDashboard" element={<CSRDashboard />} />
+        <Route path="/VendorDashboard" element={<VendorDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
